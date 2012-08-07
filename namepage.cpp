64d3ff93c736bf80e2dcf0b6e028144e5c436f8e
paalsteek <github@paalsteek.de>
2012-08-07 16:47:47 +0200

    generate name from properties

--- namepage.cpp
+++ namepage.cpp
@@ -6,8 +6,8 @@ NamePage::NamePage(QWidget *parent) :
     ui(new Ui::NamePage)
 {
     ui->setupUi(this);
-		
-		registerField("name", ui->nameEdit);
+
+        registerField("name", ui->nameEdit);
 }
 
 NamePage::~NamePage()
@@ -17,10 +17,35 @@ NamePage::~NamePage()
 
 void NamePage::initializePage()
 {
-	if ( field("name").toString().isEmpty() )
-	{
-		ui->nameEdit->setText("foobar");
-	} else {
-		ui->nameEdit->setText(field("name").toString());
-	}
+    if ( field("name").toString().isEmpty() )
+    {
+        QString name = "";
+        if ( field("weekday").toBool() && field("weekend").toBool() )
+            name += "Everyday, ";
+        else if ( field("weekday").toBool() )
+            name += "Weekdays, ";
+        else if ( field("weekend").toBool() )
+            name += "Weekend, ";
+        else
+        {
+            if ( field("monday").toBool() )
+                name += "Monday, ";
+            if ( field("tuesday").toBool() )
+                name += "Tuesday, ";
+            if ( field("wednesday").toBool() )
+                name += "Wednesday, ";
+            if ( field("thursday").toBool() )
+                name += "Thursday, ";
+            if ( field("friday").toBool() )
+                name += "Friday, ";
+            if ( field("saturday").toBool() )
+                name += "Saturday, ";
+            if ( field("sunday").toBool() )
+                name += "Sunday, ";
+        }
+        name += "at " + field("hour").toString() + ":" + field("minute").toString();
+        ui->nameEdit->setText(name);
+    } else {
+        ui->nameEdit->setText(field("name").toString());
+    }
 }
